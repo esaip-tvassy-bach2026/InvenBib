b48f45d906ae04661d5801fc17766293c0a40011
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-03-19 14:29:24 +0100

    Create modifier_livres.py
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

---
+++ modifier_livres.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+class Librairie:
+    def __init__(self):
+        try:
+            self.conn = sqlite3.connect("librairie.db")
+            self.cursor = self.conn.cursor()
+        except sqlite3.Error as e:
+            print(f"Erreur d'ouverture de la base de données : {e}")
+
+    def __del__(self):
+        if hasattr(self, 'conn'):
+            self.conn.close()
+
+    def modifier_livre(self, id, nouveau_titre, nouvel_auteur, nouvelle_annee):
+        sql = "UPDATE livres SET titre = ?, auteur = ?, annee = ? WHERE id = ?"
+        try:
+            self.cursor.execute(sql, (nouveau_titre, nouvel_auteur, nouvelle_annee, id))
+            self.conn.commit()
+            print("Livre modifié avec succès.")
+        except sqlite3.Error as e:
+            print(f"Erreur lors de la modification du livre : {e}")
+
+def test2(): // Test pour verifier que ça fonctionne
+    lib = Librairie()
+    lib.modifier_livre(1, "Animal Farm", "George Orwell", 1945)
+
+test()
