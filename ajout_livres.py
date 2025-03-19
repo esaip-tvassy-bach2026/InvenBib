eaef120a2284c62a1192eae6260255f510c3af37
Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>
2025-03-19 14:48:46 +0100

    Update ajout_livres.py
    Signed-off-by: Thomas VASSY--ROUSSEAU <zozor.web@gmail.com>

--- ajout_livres.py
+++ ajout_livres.py
@@ -8,7 +8,7 @@ class Librairie:
         # Création de la table si elle n'existe pas
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS livres (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                isbn INTEGER PRIMARY KEY,
                 titre TEXT NOT NULL,
                 auteur TEXT NOT NULL,
                 annee INTEGER
@@ -19,12 +19,12 @@ class Librairie:
     def __del__(self):
         self.conn.close()
 
-    def ajouter_livre(self, titre, auteur, annee):
+    def ajouter_livre(self, isbn, titre, auteur, annee):
         try:
             self.cursor.execute("""
-                INSERT INTO livres (titre, auteur, annee)
+                INSERT INTO livres (isbn, titre, auteur, annee)
                 VALUES (?, ?, ?)
-            """, (titre, auteur, annee))
+            """, (isbn, titre, auteur, annee))
             self.conn.commit()
             print("Livre ajouté avec succès.")
         except sqlite3.Error as e:
@@ -32,7 +32,7 @@ class Librairie:
 
 def test(): // Test pour verifier que ca marche
     lib = Librairie()
-    lib.ajouter_livre("1984", "George Orwell", 1949)
-    lib.ajouter_livre("Le Petit Prince", "Antoine de Saint-Exupéry", 1943)
+    lib.ajouter_livre(int(444444444444),"1984", "George Orwell", int(1949))
+    lib.ajouter_livre(int(676676767),"Le Petit Prince", "Antoine de Saint-Exupéry", int(1943))
 
 test()
